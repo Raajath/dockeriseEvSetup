fa653de650f7aadf7aff85bae7391f0a92077511
Raajath <rajathn02@gmail.com>
2024-03-26 16:57:59 +0530

    gave initiateRS.sh permission

--- dockerise.sh
+++ dockerise.sh
@@ -4,6 +4,7 @@ openssl rand -base64 756 > mongo-keyfile/keyfile
 docker-compose up -d --build
 
 sleep 5
+chmod 755 initiateRS.sh
 ./initiateRS.sh
 
 git clone https://github.com/Raajath/chargingInfrastructure
